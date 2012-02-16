e86084179b83788fbbcbeeffb1a732ec52b29a73
alejandrovztapia@gmail.com <alejandrovztapia@gmail.com@4244a0d0-4ece-16e4-236c-9ab2ac728048>
2012-02-16 16:55:39 +0000

    Primera vez que se ha cargado el proyecto.

---
+++ ARInside/src/com/googlecode/arinside/ARInside.java
@@ -0,0 +1,13 @@
+package com.googlecode.arinside;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ARInside extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
